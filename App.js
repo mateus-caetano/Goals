56538aab43f2bdd0b8748b2b83a51b7ff49d44c9
Mateus Caetano <mateus.caetano@alu.ufc.br>
2020-05-31 12:22:32 -0300

    category screen

--- App.js
+++ App.js
@@ -1,19 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import Category from './src/screens/category';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+  return <Category />;
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/category.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Dimensions,
+} from 'react-native';
+
+export default function Category() {
+  const data = [
+    {
+      id: 1,
+      category: 'Viagem',
+    },
+    {
+      id: 2,
+      category: 'Festa',
+    },
+    {
+      id: 3,
+      category: 'Imóvel',
+    },
+    {
+      id: 4,
+      category: 'Automóvel',
+    },
+    {
+      id: 5,
+      category: 'Pagar dívidas',
+    },
+    {
+      id: 6,
+      category: 'Empreender',
+    },
+  ];
+
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#000" barStyle="light-content" />
+      <View style={{ marginVertical: 60 }}>
+        <Text style={styles.mainText}>Agora precisamos entender seu sonho</Text>
+        <Text style={styles.subHeader}>
+          Em qual categoria ele se encaixa melhor?
+        </Text>
+      </View>
+      <ScrollView
+        style={{ flex: 1, marginBottom: 25 }}
+        showsVerticalScrollIndicator={false}
+      >
+        {data.map((category) => (
+          <View key={category.id} style={styles.listItem}>
+            <Text style={styles.subHeader}>{category.category}</Text>
+          </View>
+        ))}
+      </ScrollView>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginHorizontal: 50,
+  },
+
+  mainText: {
+    fontWeight: 'bold',
+    fontSize: 25,
+    color: '#343434',
+    lineHeight: 30,
+  },
+
+  subHeader: {
+    fontSize: 18,
+    lineHeight: 25,
+    color: '#000',
+  },
+
+  listItem: {
+    borderWidth: 1,
+    borderColor: '#DBDBDB',
+    borderRadius: 15,
+    height: 60,
+    width: Dimensions.get('window').width - 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginVertical: 10,
+  },
+});
